dd1df4f9a1e010214437a47d3fe26490fca05d88
tonderys <tonszym@gmail.com>
2022-09-30 19:21:00 +0200

    fixed testcase name

--- tests/WithJoints_test.py
+++ tests/WithJoints_test.py
@@ -13,7 +13,7 @@ class WithJoints_tests(unittest.TestCase):
         assert(with_joints.box.outer.width == 7.5)
         assert(with_joints.box.outer.height == 7.5)
 
-    def test_enlarging_the_box_to_46x60(self):
+    def test_enlarging_the_box_to_45x59(self):
         box = EmptyBox(45,59,1);
         with_joints = WithJoints(box)
         assert(with_joints.box.outer.width == 52.5)
