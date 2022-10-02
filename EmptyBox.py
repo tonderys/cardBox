72fa843ca0d79f84af364e8f53da56086b0af495
tonderys <tonszym@gmail.com>
2022-10-02 08:09:13 +0200

    added chamfer to EmptyBox to make putting the cards in easier

--- EmptyBox.py
+++ EmptyBox.py
@@ -29,14 +29,32 @@ class EmptyBox:
     def get_wall_z(self) -> float:
         return (self.outer.depth - self.inner.depth) / 2
 
+    def _get_chamfer(self) -> OpenSCADObject:
+        return polyhedron(points=[[self.get_wall_x(), self.get_wall_y(), self.outer.depth * 0.8],
+                                  [self.get_wall_x(), self.outer.height - self.get_wall_y(), self.outer.depth * 0.8],
+                                  [self.outer.width - self.get_wall_x(), self.outer.height - self.get_wall_y(), self.outer.depth * 0.8],
+                                  [self.outer.width - self.get_wall_x(), self.get_wall_y(), self.outer.depth * 0.8],
+                                  [self.get_wall_x() / 2, self.get_wall_y() / 2, self.outer.depth],
+                                  [self.get_wall_x() / 2, self.outer.height - (self.get_wall_y() / 2), self.outer.depth],
+                                  [self.outer.width - (self.get_wall_x() / 2), self.outer.height - (self.get_wall_y() / 2), self.outer.depth],
+                                  [self.outer.width - (self.get_wall_x() / 2), self.get_wall_y() / 2, self.outer.depth]],
+                          faces=[[0, 1, 2, 3],
+                                 [4, 5, 1, 0],
+                                 [5, 6, 2, 1],
+                                 [6, 7, 3, 2],
+                                 [7, 4, 0, 3],
+                                 [7, 6, 5, 4]])
+
+    def _get_chamfered_outer(self) -> OpenSCADObject:
+        return difference()(self.outer.scad(), self._get_chamfer())
+
     def get_roof(self) -> OpenSCADObject:
-        roof = up(self.inner.depth)(cube([self.inner.width,self.inner.height, self.get_wall_z()]))
-        return translate([self.get_wall_x(),
-                          self.get_wall_y(),
-                          self.get_wall_z()])(roof)
+        roof = up(self.outer.depth - self.get_wall_z())(cube([self.outer.width, self.outer.height, self.get_wall_z()]))
+        return difference()(roof, self._get_chamfered_outer())
 
     def scad(self) -> OpenSCADObject:
-        outer = difference()(self.outer.scad(),self.get_roof())
+        outer = self._get_chamfered_outer()
+
         inner = translate((self.get_wall_x(),
                            self.get_wall_y(),
                            self.get_wall_z()))(self.inner.scad())
