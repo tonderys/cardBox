27c93d207ca0ffd1536d59f04a202ed32d95fb6b
tonderys <tonszym@gmail.com>
2022-10-07 23:04:56 +0200

    finished first step of Fillet refactoring

--- Fillet.py
+++ Fillet.py
@@ -8,42 +8,24 @@ def get_all_combinations(delta_x, delta_y, delta_z, obj):
 
 class Fillet:
     def __init__(self, obj: MeasuredObject, r: float):
-        corner = sphere(r = r, segments = 20)
-
         height = obj.get_height() - (2 * r)
         width = obj.get_width() - (2 * r)
         depth = obj.get_depth() - (2 * r)
 
+        corner = sphere(r = r, segments = 20)
         corners = get_all_combinations([0,width], [0, height], [0,depth], corner)
-        # corners = [translate([0    , 0     , 0    ])(corner),
-        #            translate([0    , 0     , depth])(corner),
-        #            translate([width, 0     , 0    ])(corner),
-        #            translate([0    , height, 0    ])(corner),
-        #            translate([width, height, 0    ])(corner),
-        #            translate([width, 0     , depth])(corner),
-        #            translate([0    , height, depth])(corner),
-        #            translate([width, height, depth])(corner)]
         self.corners = [translate([r,r,r])(c) for c in corners]
 
         column_z = cylinder(r=r, h=depth, segments = 20)
-        up_columns = [column_z,
-                      translate([width, height, 0])(column_z),
-                      translate([width, 0     , 0])(column_z),
-                      translate([0    , height, 0])(column_z)]
+        up_columns = get_all_combinations([0, width], [0, height], [0], column_z)
         self.z_columns = [translate([r,r,r])(uc) for uc in up_columns]
 
         column_x = rot_z_to_x(cylinder(r=r, h=width, segments = 20))
-        x_columns = [column_x,
-                      translate([0, 0     , depth])(column_x),
-                      translate([0, height, 0    ])(column_x),
-                      translate([0, height, depth])(column_x)]
+        x_columns = get_all_combinations([0], [0, height], [0, depth], column_x)
         self.x_columns = [translate([r,r,r])(uc) for uc in x_columns]
 
         column_y = rot_z_to_y(cylinder(r=r, h=height, segments = 20))
-        y_columns = [column_y,
-                      translate([width, 0, 0    ])(column_y),
-                      translate([0    , 0, depth])(column_y),
-                      translate([width, 0, depth])(column_y)]
+        y_columns = get_all_combinations([0, width], [0], [0, depth], column_y)
         self.y_columns = [translate([r,r,r])(uc) for uc in y_columns]
 
         height = obj.get_height() - r
@@ -51,23 +33,13 @@ class Fillet:
         depth = obj.get_depth() - r
 
         cube_x = cube([obj.get_width(),r,r])
-        self.x_cubes = [cube_x,
-                        translate([0, 0     , depth])(cube_x),
-                        translate([0, height, 0    ])(cube_x),
-                        translate([0, height, depth])(cube_x)]
+        self.x_cubes = get_all_combinations([0], [0, height], [0, depth], cube_x)
 
         cube_y = cube([r,obj.get_height(),r])
-        self.y_cubes = [cube_y,
-                        translate([width, 0, 0    ])(cube_y),
-                        translate([width, 0, depth])(cube_y),
-                        translate([0    , 0, depth])(cube_y)]
+        self.y_cubes = get_all_combinations([0, width], [0], [0, depth], cube_y)
 
         cube_z = cube([r,r,obj.get_depth()])
-        self.z_cubes = [cube_z,
-                        translate([width, 0     , 0])(cube_z),
-                        translate([0    , height, 0])(cube_z),
-                        translate([width, height, 0])(cube_z)]
-
+        self.z_cubes = get_all_combinations([0, width], [0, height], [0], cube_z)
 
     def scad(self) -> OpenSCADObject:
         return difference()(union()([c for c in self.x_cubes], [c for c in self.y_cubes], [c for c in self.z_cubes]),
