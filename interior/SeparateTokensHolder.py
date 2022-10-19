e8b2c0f2c0746b4739997f8af31160f448075147
tonderys <tonszym@gmail.com>
2022-10-19 08:27:57 +0200

    changed SeparateTokensHolder to make paths only for tokens, not for separators. It still works only for some simple shapes like circle or square

--- interior/SeparateTokensHolder.py
+++ interior/SeparateTokensHolder.py
@@ -16,10 +16,14 @@ class SeparateTokensHolder(Interior):
         height = (token.get_depth() + separator)
         Interior.__init__(self, "Separate Tokens Holder", token.get_width(), height * amount, depth)
 
-        tokens = up(token.get_height())(rot_z_to_y(union()([up(height * i)(token.scad()) for i in range(amount)])))
-        path = up(token.get_height() / 2)(cube([token.get_width(), height * amount, depth - token.get_height() / 2]))
-        self.body = union()(tokens, path)
+        path = forward(token.get_height() / 2)(cube([token.get_width(), token.get_height() / 2, token.get_depth()]))
+        high_token = union()(token.scad(), path)
+        tokens = forward(height * amount)(
+                    rot_z_to_neg_y(
+                        union()(
+                            [up(height * i)(high_token) for i in range(amount)])))
+        self.body = tokens
 
 if __name__ == '__main__':
-    obj = SeparateTokensHolder(Round(23,2,8),29,23)
+    obj = SeparateTokensHolder(Round(23,2,16),3,23)
     scad_render_to_file(obj.scad(), f"f:\\Druk3D\\STL\\openSCAD\\test.scad")
