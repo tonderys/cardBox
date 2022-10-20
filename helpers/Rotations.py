27e747f7708c16a6fe83a2345469a6310a1f4d9d
tonderys <tonszym@gmail.com>
2022-10-20 08:21:29 +0200

    added first step of rotations around objects center that I'll need for the refactor

---
+++ helpers/Rotations.py
@@ -0,0 +1,38 @@
+from solid import *
+
+from parametricBox.helpers.Measured import *
+
+def move_to_middle(object):
+    object.body = translate([-object.get_width() / 2, -object.get_height() / 2, -object.get_depth() / 2])(object.body)
+
+def move_back(object):
+    object.body = translate([object.get_width() / 2, object.get_height() / 2, object.get_depth() / 2])(object.body)
+
+def exchange_width_and_height(object):
+    object.width, object.height = object.height, object.width
+
+def exchange_depth_and_width(object):
+    object.depth, object.width = object.width, object.depth
+
+def exchange_depth_and_height(object):
+    object.depth, object.height = object.height, object.depth
+
+angle = 90 #I'll do the math with other angles in step#2
+
+def rot_z(object: Measured):
+    move_to_middle(object)
+    object.body = rotate(a=angle, v=[0,0,1])(object.body)
+    exchange_width_and_height(object)
+    move_back(object)
+
+def rot_y(object: Measured):
+    move_to_middle(object)
+    object.body = rotate(a=angle, v=[0,1,0])(object.body)
+    exchange_depth_and_width(object)
+    move_back(object)
+
+def rot_x(object: Measured):
+    move_to_middle(object)
+    object.body = rotate(a=angle, v=[1,0,0])(object.body)
+    exchange_depth_and_height(object)
+    move_back(object)
